1af4b42274633dc5115519a4a02c5983e6f1e3f8
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-07 22:02:51 +0100

    Header styling added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ export default function App() {
   return (
     <div className="App">
       <header className="App__header">
-        <div className="row">
-          <div className="col-md-6">
-            <h1 className="App__header--logo">[ˈdɪkʃ(ə)n(ə)ri]</h1>
-          </div>
-          <div className="col-md-6">
-            <h1 className="App__header--title">Dictionary</h1>
-          </div>
-        </div>
+        <div className="App__header--logo">[ˈdɪkʃ(ə)n(ə)ri]</div>
+        <h1 className="App__header--subtitle">
+          online dictionary and synonym application
+        </h1>
       </header>
     </div>
   );
