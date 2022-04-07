9a6be9fad4b54dbf5bbf58dcd6cac907f4fab724
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-07 21:04:09 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App__header">
+        Hello
+      </header>
+    </div>
+  );
+}
