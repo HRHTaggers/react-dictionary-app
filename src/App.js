59136d7776d1a6a77d4d09d16057a03983b6b83f
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-07 21:37:08 +0100

    Placeholder for header input

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ export default function App() {
   return (
     <div className="App">
       <header className="App__header">
-        Hello
+        <div className="row">
+          <div className="col-md-6">
+            <h1 className="App__header--logo">[ˈdɪkʃ(ə)n(ə)ri]</h1>
+          </div>
+          <div className="col-md-6">
+            <h1 className="App__header--title">Dictionary</h1>
+          </div>
+        </div>
       </header>
     </div>
   );
