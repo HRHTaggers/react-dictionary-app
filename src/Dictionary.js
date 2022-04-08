7576e51fcef8639185128882bb63418490359277
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-08 21:27:50 +0100

    Added components to call word, definition & POS from API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,19 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [word, setWord] = useState("");
+    let [results, setResults] = useState("");
 
     function handleWord (event) {
         setWord(event.target.value);
     }
 
     function handleResponse(response) {
-        console.log(response.data[0]);
+        console.log(response.data[0].meanings[0].definitions[0].definition);
+        setResults(response.data[0]);
     }
 
     function search(event) {
@@ -27,6 +30,7 @@ export default function Dictionary() {
             <form onSubmit={search}>
                 <input type="search" placeholder="Enter word" onChange={handleWord} />
             </form>
+            <Results results={results}/>
         </div>
     );
 }
