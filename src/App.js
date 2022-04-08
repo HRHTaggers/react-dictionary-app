e2afea381ba614bc7186907313d8ba6573e02aac
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-04-08 19:54:42 +0100

    Added footer and removed unnecessary icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import './App.css';
+import logo from "./logo.png";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App__header">
-        <div className="App__header--logo">[ˈdɪkʃ(ə)n(ə)ri]</div>
-        <h1 className="App__header--subtitle">
-          online dictionary and synonym application
-        </h1>
-      </header>
+      <div className="container-fluid">
+        <header className="App__header">
+          <div className="App__header--logo">[ˈdɪkʃ(ə)n(ə)ri]</div>
+          <h1 className="App__header--subtitle">
+            online dictionary and synonym application
+          </h1>
+        </header>
+        <footer className="App__footer">
+          <div className="App__footer--IPA">
+            <strong>
+              ˈkəʊdɪd baɪ eɪʧ ɑː taʎ(ː)aˈrini, ˈəʊpən-sɔːs ɒn ˈgɪthɅb ænd
+              ˈhəʊstɪd baɪ ˈnetlɪfaɪ
+            </strong>
+          </div>
+          <div className="App__footer--English">
+            <small>
+              Coded by H R Tagliarini, open-sourced on GitHub and hosted by
+              Netlify
+            </small>
+          </div>
+          <div>
+            <a
+              href="https://htcodes.netlify.app/"
+              target="blank"
+              rel="noopener noreferrer"
+            >
+              <img className="App__footer--logo" src={logo} alt="HTCode logo" />
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
